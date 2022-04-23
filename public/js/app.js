92377bbf972c02d7464a4d0d12fc7a0329f12c58
Archit Dashora <architdashora@Archits-MacBook-Air.local>
2022-04-23 07:20:03 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,42 @@
+
+console.log('Client')
+// fetch('http://localhost:3000/weather?address=boston').then((response)=>{
+//     response.json().then((data) => {
+//         if(data.msg){
+//             console.log(data.msg)
+//         }
+//         else{
+//             console.log(data.location)
+//             console.log("temperature is " + data.forecast.temperature)
+//         }
+        
+    
+//     })
+// })
+const weatherForm=document.querySelector('form')
+const search=document.querySelector('input')
+const message11=document.querySelector('#One')
+const message22=document.querySelector('#Two')
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location=search.value
+    message11.textContent='Loading...'
+    message22.textContent=''
+    fetch('http://localhost:3000/weather?address='+ encodeURIComponent(location)).then((response)=>{
+    response.json().then((data) => {
+        if(data.msg){
+            // console.log(data.msg)
+            message11.textContent=data.msg
+        }
+        else{
+            message11.textContent=data.location
+            message22.textContent="temperature is " + data.forecast.temperature
+            // console.log(data.location)
+            // console.log("temperature is " + data.forecast.temperature)
+        }
+        
+    
+    })
+})
+    // console.log('testing')
+})
---
+++ src/app.js
@@ -0,0 +1,83 @@
+const path=require('path')
+const hbs=require('hbs')
+const express=require('express')
+const request=require('request')
+const geocode=require('../../weather-app/utils/geocode')
+// const geocode=require('../../weather-app/utils/geocode')
+const forecast=require('../../weather-app/utils/forecast')
+
+
+const app=express()
+const location=path.join(__dirname,'../public')
+const viewPath=path.join(__dirname,'../templates/views')
+const partialsPath=path.join(__dirname,'../templates/partials')
+
+app.set('view engine','hbs')
+app.set('views', viewPath);
+hbs.registerPartials(partialsPath)
+// app.set('public',path.join(__dirname,'../public'))
+// const locate=path.join(__dirname,'../views')
+app.use(express.static(location))
+// app.use(express.static(locate))
+
+// app.use(express.static(location))
+
+app.get('',(req,res)=>{
+    res.send('Hello Express!')
+})
+app.get('/help',(req ,res)=>{
+    res.render('help',{
+        message1: 'myself archit i am here for your help',
+        title: 'help'
+    })
+    // res.send('<h1>about this page</h1>')
+
+
+})
+app.get('/about',(req,res)=>{
+    res.send('<h1>about this page</h1>')
+})
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            msg: 'Please enter the address'
+        })
+    }
+    // res.send({
+        geocode.geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+            if(error){
+                return res.send({
+                 msg:error
+                })
+         
+            } 
+           
+             forecast.forecast(latitude,longitude,(error,foredata)=>{
+                 if(error){
+                     return res.send({
+                         msg:error
+                     })
+                 }
+                //  console.log(location)
+                //  console.log(foredata)
+                res.send({
+                    location:req.query.address,
+                    forecast:foredata
+                })
+             })
+         })
+    // })
+})
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        msg: 'Help article not found'
+    })
+})
+app.get('*',(req,res)=>{
+    res.render('404',{
+        msg: 'Page not found'
+    })
+})
+app.listen(3000,()=>{
+    console.log('This website is on port 3000')
+})
